9f883d0d3e7be0a19065f6848b0a4680c0750f99
omkar <omkar@omkar-PC>
2020-01-29 12:12:36 +0530

    arrays demo

---
+++ 4.ArrayDemo/src/ArrayDemo.java
@@ -0,0 +1,15 @@
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] anArray;
+		anArray = new int[3];
+		anArray[0] = 100;
+		anArray[1] = 200;
+		anArray[2] = 300;
+
+		System.out.println("  at index0   " + anArray[0] + "  element1  " + anArray[1] + "  element2  " + anArray[2]);
+	}
+
+}
---
+++ 4.ArrayDemo/src/MultiArray.java
@@ -0,0 +1,9 @@
+
+public class MultiArray {
+	public static void main(String[] args) {
+		String[][] names = { { "mr. ", "ms. ", "mrs." }, { "smith", "clarke" } };
+		// System.out.println(names[0][0] + names[1][0]);
+		//System.out.println(names[0][1] + names[1][0]);
+		System.out.println(names[0][2]+names[1][1]);
+	}
+}
